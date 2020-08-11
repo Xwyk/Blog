ba1bc3e36e0269948c845d4a46b2340add1f948e
Florian LEBOUL <florianleboul@gmail.com>
2020-08-11 15:09:45 +0200

    indentation correction in registercontroller

--- Controller/RegisterController.php
+++ Controller/RegisterController.php
@@ -7,17 +7,16 @@ use Blog\Model\User;
 use Blog\Model\Manager\UserManager;
 
 class RegisterController extends Controller
-{
-    
+{   
     public function register()
     {
         try {
             $userToAdd = new User([
                 'firstName'   => filter_input(INPUT_POST, 'firstName', FILTER_SANITIZE_FULL_SPECIAL_CHARS),
-                'lastName'    => filter_input(INPUT_POST, 'lastName', FILTER_SANITIZE_FULL_SPECIAL_CHARS),
-                'pseudo'      => filter_input(INPUT_POST, 'pseudo', FILTER_SANITIZE_FULL_SPECIAL_CHARS),
-                'mailAddress' => filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL),
-                'password'    => filter_input(INPUT_POST, 'password', FILTER_SANITIZE_FULL_SPECIAL_CHARS)
+                'lastName'    => filter_input(INPUT_POST, 'lastName' , FILTER_SANITIZE_FULL_SPECIAL_CHARS),
+                'pseudo'      => filter_input(INPUT_POST, 'pseudo'   , FILTER_SANITIZE_FULL_SPECIAL_CHARS),
+                'mailAddress' => filter_input(INPUT_POST, 'email'    , FILTER_SANITIZE_EMAIL),
+                'password'    => filter_input(INPUT_POST, 'password' , FILTER_SANITIZE_FULL_SPECIAL_CHARS)
             ]);
             (new UserManager($this->config))->add($userToAdd);
             $this->redirect($this::URL_LOGIN);
