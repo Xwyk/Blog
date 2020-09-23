0f953822b45d141ccc6676697bf9aeedbd192760
florianleboul@gmail.com <florianleboul@gmail.com>
2020-09-23 07:56:01 +0200

    HTML hrefs configured with router.url method

--- Controller/HomeController.php
+++ Controller/HomeController.php
@@ -18,10 +18,10 @@ class HomeController extends Controller
     {
         //Get posts list
         $articles = (new PostManager($this->config))->getAll();
-        if ($_GET['action'] == 'login') {
-        $this->redirectTo("login_page");
-            # code...
-        }
+        // if ($_GET['action'] == 'login') {
+        // $this->redirectTo("login_page");
+        //     # code...
+        // }
         //Dipslay Home view
         // return $this->namedRedirect();
         return $this->render($this::VIEW_HOME, ['articles' => $articles]);
