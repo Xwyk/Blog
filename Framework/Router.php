3734124d08e4c39377d23b41b6fe3a362bf04fda
Florian LEBOUL <florianleboul@gmail.com>
2020-11-11 18:07:01 +0100

    Codacy code style update

--- Framework/Router.php
+++ Framework/Router.php
@@ -40,11 +40,11 @@ class Router
     }
 
     public function run($view, $session, $config){
-        if(!isset($this->routes[$_SERVER['REQUEST_METHOD']])){
+        if(!isset($this->routes[filter_input(INPUT_SERVER,'REQUEST_METHOD')])){
             throw new RouterException('REQUEST_METHOD does not exist');
         }
 
-        foreach($this->routes[$_SERVER['REQUEST_METHOD']] as $route){
+        foreach($this->routes[filter_input(INPUT_SERVER,'REQUEST_METHOD')] as $route){
             if($route->match($this->url)){
                 return $route->call($view, $session, $config, $this);
             }
